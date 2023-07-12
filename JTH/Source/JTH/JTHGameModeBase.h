e35ceceddde968482866b226ac40e861891b0436
태훈 정 <c0t0h0@naver.com>
2023-07-12 17:56:31 +0900

    test

---
+++ JTH/Source/JTH/JTHGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JTHGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JTH_API AJTHGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
