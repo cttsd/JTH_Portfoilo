0a290399646d97258a0203bdd778207f4485fc25
태훈 정 <c0t0h0@naver.com>
2023-07-18 06:45:52 +0900

    test

--- JTH/Source/JTH/MyPlayer/MyAnimInstance.cpp
+++ JTH/Source/JTH/MyPlayer/MyAnimInstance.cpp
@@ -2,4 +2,90 @@
 
 
 #include "MyAnimInstance.h"
+#include "MyCharacter.h"
+
+void UMyAnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	// 나자신이 애님인스턴스입니다.
+	// OnMontageEnded.AddDynamic(this, &UTPSAnimInstance::MontageEnd);
+
+	// 아이들       => 공격
+	// 브랜드 아웃     블랜드인
+
+	OnMontageBlendingOut.AddDynamic(this, &UMyAnimInstance::MontageEnd);
+
+	// 나를 가진 액터를 여기서 가져옵니다.
+	// TPS캐릭터에게 다 세팅을 해줬다.
+
+	AMyCharacter* Chracter = Cast<AMyCharacter>(GetOwningActor());
+
+	if (nullptr == Chracter && false == Chracter->IsValidLowLevel())
+	{
+		return;
+	}
+
+	AllAnimations = Chracter->AllAnimations;
+}
+
+// 시작할때 한번 업데이트를 돌려주므로.
+void UMyAnimInstance::NativeUpdateAnimation(float _DeltaTime)
+{
+	Super::NativeUpdateAnimation(_DeltaTime);
+
+	if (0 == AllAnimations.Num())
+	{
+		return;
+	}
+
+	AMyCharacter* Chracter = Cast<AMyCharacter>(GetOwningActor());
+
+	if (nullptr == Chracter && false == Chracter->IsValidLowLevel())
+	{
+		return;
+	}
+
+	AniState = Chracter->AniState;
+
+	class UAnimMontage* Montage = AllAnimations[AniState];
+
+	if (nullptr == Montage)
+	{
+		return;
+	}
+
+	if (false == Montage_IsPlaying(Montage))
+	{
+		Montage_Play(Montage, 1.0f);
+	}
+}
+
+void UMyAnimInstance::MontageEnd(UAnimMontage* Anim, bool _Inter)
+{
+	TSubclassOf<UAnimInstance> Inst = UMyAnimInstance::StaticClass();
+
+	AMyCharacter* Chracter = Cast<AMyCharacter>(GetOwningActor());
+
+	if (nullptr == Chracter && false == Chracter->IsValidLowLevel())
+	{
+		return;
+	}
+
+	// Anim 종료된 몽타주
+	if (AllAnimations[TPSAniState::Attack] == Anim)
+	{
+		AniState = TPSAniState::Idle;
+		Chracter->AniState = AniState;
+		Montage_Play(AllAnimations[TPSAniState::Idle], 1.0f);
+	}
+
+	if (AllAnimations[TPSAniState::Jump] == Anim)
+	{
+		AniState = TPSAniState::Idle;
+		Chracter->AniState = AniState;
+		Montage_Play(AllAnimations[TPSAniState::Idle], 1.0f);
+	}
+
+}
 
--- JTH/Source/JTH/MyPlayer/MyAnimInstance.h
+++ JTH/Source/JTH/MyPlayer/MyAnimInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "MyEnums.h"
 #include "MyAnimInstance.generated.h"
 
 /**
@@ -13,5 +14,18 @@ UCLASS()
 class JTH_API UMyAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+	UPROPERTY(Category = "GameModeValue", EditAnywhere, BlueprintReadWrite)
+		TPSAniState AniState = TPSAniState::Idle;
+
+	TMap<TPSAniState, class UAnimMontage*> AllAnimations;
+
+	UFUNCTION()
+		void MontageEnd(UAnimMontage* Anim, bool _Inter);
+
+	// 애님인스턴용 Tick과 BeginPlay
+
+protected:
+	void NativeBeginPlay() override;
+	void NativeUpdateAnimation(float _DeltaTime) override;
 };
