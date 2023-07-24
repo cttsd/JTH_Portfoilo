556e2687b3e2e06459b17b8e28c591512e358356
태훈 정 <c0t0h0@naver.com>
2023-07-24 18:35:29 +0900

    test

---
+++ JTH/Source/JTH/AI/BTTask_AIBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_AIBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JTH_API UBTTask_AIBase : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
