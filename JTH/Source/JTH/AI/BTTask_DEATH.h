221fd0ac1bc76e23fa8b561f36cde3e4a3841c2c
태훈 정 <c0t0h0@naver.com>
2023-07-24 10:48:03 +0900

    test

---
+++ JTH/Source/JTH/AI/BTTask_DEATH.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BTTask_DEATH.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JTH_API UBTTask_DEATH : public UObject
+{
+	GENERATED_BODY()
+	
+};
